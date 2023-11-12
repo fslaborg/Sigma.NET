e58e4938d652123677ceddd1b48461bf6b0fd3ad
Timo Mühlhaus <timo.muehlhaus@rptu.de>
2023-11-12 20:01:19 +0100

    Add json test data

--- src/sigmaNET/SigmaNET.fsx
+++ src/sigmaNET/SigmaNET.fsx
@@ -7,33 +7,17 @@ open System
 open sigmaNET
 open Newtonsoft.Json
 
-let node1 = Node.Init("Thomas",X = 0, Y = 10, Size = 10, Label = "A" )
-let node2 = Node.Init("Eric",X = 10, Y = 0, Size = 50, Label = "B" )
-let edge1 = Edge.Init("Thomas","Eric", Size = 10)
+open Newtonsoft.Json.Linq
+open System.IO
 
-let graphData = GraphData()
-let _ = graphData.addNode(node1)
-let _ = graphData.addNode(node2)
-let _ = graphData.addEdge(edge1)
+let jsonTxt = File.ReadAllText("D:/Source/sigmaNET/src/sigmaNET/data.json")
+let data = JsonConvert.DeserializeObject<GraphData>(jsonTxt)
 
-// let node_tmp = Node.Init("Eric" )
-
-// let ntsettings = new JsonSerializerSettings()
-// //let _ = ntsettings.PreserveReferencesHandling <- PreserveReferencesHandling.Objects
-// let _ = ntsettings.ReferenceLoopHandling <- ReferenceLoopHandling.Ignore//ReferenceLoopHandling.Serialize
-
-// JsonConvert.SerializeObject (edge1,ntsettings,)
-//HTML.show(graphData)
 
 Graph.empty()
-|> Graph.withNodes [ 
-        //node "Thomas" 
-        Node.Init("Thomas",X = 0, Y = 10, Size = 10, Label = "A" )
-        Node.Init("Eric",X = 10, Y = 0, Size = 50, Label = "B" )
-    ]
-|> Graph.withEdge (Edge.Init("Thomas","Eric", Size = 10))
+|> Graph.withNodes data.Nodes
+|> Graph.withEdges data.Edges
 |> Graph.show()
 
 
-InternalUtils.getFullGraphologyLibraryJS()
 
