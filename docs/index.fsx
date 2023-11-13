2ca7a6bbf3778f28019d6a5b5afa3af0add5a144
Timo Mühlhaus <timo.muehlhaus@rptu.de>
2023-11-13 12:45:22 +0100

    Add build chain

---
+++ docs/index.fsx
@@ -0,0 +1,41 @@
+(**
+---
+title: Getting Started
+category: sigmaNET 
+categoryindex: 1
+index: 1 
+---
+
+# sigmaNET
+
+The sigmaNET project aims to provide a rigorous and performant tool for Network Science. 
+It is aimed at anyone who works with Graphs/Networks and does not require a strong knowledge of F# to get started. 
+
+# Getting Started 
+
+## Prerequisites 
+
+To set up a dev environment, we recommend [VSCode](https://code.visualstudio.com/) with the [Ionide](https://marketplace.visualstudio.com/items?itemName=Ionide.Ionide-fsharp) plugin  
+You will also need [DotNet 6](https://dotnet.microsoft.com/en-us/download/dotnet/6.0) installed.
+
+The library is designed primarily for use in an fsharp scripting environment using .fsx files. 
+But it also works well in [notebooks](https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.dotnet-interactive-vscode).
+This [video](https://www.youtube.com/watch?v=1ROKvmcOloo&list=PLdo4fOcmZ0oUFghYOp89baYFBTGxUkC7Z&index=3) has a good walk through of setting your environment. 
+
+# Contributing and copyright
+
+The project is hosted on [GitHub][gh] where you can [report issues][issues], fork 
+the project and submit pull requests. If you're adding a new public API, please also 
+consider adding [samples][docs] that can be turned into a documentation. You might
+also want to read the [library design notes][readme] to understand how it works.
+
+The library is available under the OSI-approved MIT license, which allows modification and 
+redistribution for both commercial and non-commercial purposes. For more information see the 
+[License file][license] in the GitHub repository. 
+
+  [docs]: https://github.com/fslaborg/sigmaNET/tree/main/docs
+  [gh]: https://github.com/fslaborg/sigmaNET
+  [issues]: https://github.com/fslaborg/sigmaNET/issues
+  [readme]: https://github.com/fslaborg/sigmaNET/blob/main/README.md
+  [license]: https://github.com/fslaborg/sigmaNET/blob/main/LICENSE
+*)
--- src/sigmaNET/SigmaNET.fsx
+++ src/sigmaNET/SigmaNET.fsx
@@ -3,38 +3,42 @@
 #r "nuget: Giraffe.ViewEngine, 1.4.0"
 #r "./bin/Debug/net6.0/sigmaNET.dll"
 
-#r "nuget: Graphoscope, 0.4.0"
-open Graphoscope
+#r "nuget: sigmaNET, 0.4.0"
+open sigmaNET
 
 open System
 open System.IO
 open Newtonsoft.Json
 open sigmaNET
 
-open Graphoscope.RandomModels
+open sigmaNET.RandomModels
 
+let colors = [|"#F2FFE9";"#A6CF98";"#557C55";"#FA7070"|]
 
 let N = 900
 let p = 0.005
 let rnd = new Random()
 let random_number = rnd.Next(1, 51)
-let myGilbertGraph = Gilbert.initDirectedFGraph N p
+//let myGilbertGraph = Gilbert.initDirectedFGraph N p
+let myBollobasRiordan = RandomModels.BollobasRiordan.initDirectedFGraph  N 0.5 0.1 0.4 0.6 0.4 FGraph.empty
 
 Graph.empty()
 |> Graph.withNodes[
-    for node in myGilbertGraph do
-        yield (Node.Init(key=string node.Key, Size=rnd.Next(1, 21)))
+    for node in myBollobasRiordan do
+        yield (Node.Init(key=string node.Key, Size=rnd.Next(1, 18), Color=colors.[rnd.Next(0, 4)]))
 
 ]
 |> Graph.withEdges [
-    for node in myGilbertGraph do
+    for node in myBollobasRiordan do
         let pred,t,_ = node.Value
         for kv in pred do
-            yield (Edge.Init(source=string kv.Key, target=string t,Size=rnd.Next(1, 6)) )
+            yield (Edge.Init(source=string kv.Key, target=string t,Size=rnd.Next(1, 3)) )
 
 ]
-//|> Graph.withForceAtlas2(Iterations=100,Settings=FA2Settings.Init(AdjustSizes=true,Gravity=5),GetEdgeWeight="size")
-|> Graph.withNoverlap(50)
+
+|> Graph.withForceAtlas2(Iterations=100,Settings=FA2Settings.Init(AdjustSizes=true,Gravity=5),GetEdgeWeight="size")
+// |> Graph.withNoverlap(50)
+// |> Graph.withCircularLayout()
 |> Graph.show()
 
 
@@ -51,3 +55,17 @@ Graph.empty()
 
 
 
+// SigmaSetting:
+//     settings: {
+//     sideMargin: 10,
+//     minEdgeSize: 2,
+//     maxEdgeSize: 2,
+//     minNodeSize: 3,
+//     maxNodeSize: 14,
+//     labelThreshold: 2,
+//     labelAlignment: 'center',
+//     nodesPowRatio: 1.3,
+//     edgesPowRatio: 1,
+//     autoResize: true,
+//     autoRescale: true,
+//     labelSizeRatio: 20,
